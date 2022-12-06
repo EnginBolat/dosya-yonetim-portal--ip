4e94dde921ec6fdc8ccc70aebd40ff6eb8f394b5
Engin Bolat <engi.bolat@gmail.com>
2022-12-06 09:12:55 +0300

    Profile page id paramters added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { PanelComponent } from './components/panel/panel.component';
-import { CanActivate } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './services/auth.guard';
-import { UyeComponent } from './components/uye/uye.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {
+    path: 'profile/:profileId',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'panel', component: PanelComponent, canActivate: [AuthGuard] },
 ];
 
